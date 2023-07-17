a0ea05a000e6ac633cfaa54974e9933e6069425a
batyr <mansurmekin@gmail.com>
2023-07-18 01:13:22 +0600

    fix

--- src/adapters/db/main.py
+++ src/adapters/db/main.py
@@ -1,8 +1,12 @@
 import contextlib
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
-                                    async_sessionmaker, create_async_engine)
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 from .config import DBConfig
 
