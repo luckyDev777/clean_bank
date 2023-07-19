09e126f32960e27c1a1ca282d685d8a6b5270579
batyr <mansurmekin@gmail.com>
2023-07-19 23:04:11 +0600

    add example test

--- src/presentation/api/controllers/main.py
+++ src/presentation/api/controllers/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from .customer import router as customer_router
+from .root import router as root_router
 from .exceptions import setup_exception_handlers
 
 
 def setup_controllers(app: FastAPI) -> None:
+    app.include_router(router=root_router)
     app.include_router(router=customer_router)
 
     setup_exception_handlers(app=app)
---
+++ src/presentation/api/controllers/root.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/health_check", tags=["root"])
+
+
+@router.get(
+    path="/"
+)
+async def read_root() -> dict:
+    return {"Hello": "World"}
